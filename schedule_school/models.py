3d96c11b9f069418ca47f9610ba5b0ea15039848
logerk46 <logerk15a@gmail.com>
2018-11-10 12:26:41 +0200

    db migration

--- schedule_school/models.py
+++ schedule_school/models.py
@@ -6,6 +6,7 @@ class Schedule(models.Model):
 	id = models.AutoField(primary_key=True)
 	room_number = models.IntegerField()
 	id_teacher = models.IntegerField()
+	id_class = models.IntegerField()
 	id_subject = models.IntegerField()
 	id_day = models.IntegerField()
 	id_lesson_number = models.IntegerField()
@@ -14,18 +15,30 @@ class Class_Name(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.TextField(max_length=255)
 
+	def __str__(self):
+		return self.name
+
 class Teacher(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.TextField(max_length=255)
 	id_block = models.IntegerField()
+
+	def __str__(self):
+		return self.name
 	
 class Subject(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.TextField(max_length=255)
 
+	def __str__(self):
+		return self.name
+
 class Day(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.TextField(max_length=255)
+
+	def __str__(self):
+		return self.name
 
 
 
