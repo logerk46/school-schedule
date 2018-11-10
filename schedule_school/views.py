8ce2675ed4d9de42fd426480ec6fdb5e499be985
logerk46 <logerk15a@gmail.com>
2018-11-10 13:10:12 +0200

    make skeleton view

--- schedule_school/views.py
+++ schedule_school/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from . import models
 
 # Create your views here.
 def list(request):
-	return HttpResponse("list")
+
+	if('day' in request.GET and 'class_name' in request.GET):
+		rday = request.GET['day']
+		rclass = request.GET['class_name']
+		classn = models.Class_Name.objects.all().filter(name=rclass)
+		Schedule = models.Schedule.objects.all().filter(id_day=rday)
+		return HttpResponse(Schedule)
+	else:
+		return HttpResponse(f"Not enough parametrs")
