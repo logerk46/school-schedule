16f04670e4fe6e5cac69543d8974f237ad3ba25c
melancholiaque <melancholique655312@gmail.com>
2018-11-10 13:20:21 +0200

    view

--- schedule_school/views.py
+++ schedule_school/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse, JsonResponse
 from . import models
 
 # Create your views here.
+
+def print_schedule(s):
+    return (f"[{s.room_number} {s.id_teacher} {s.id_class} {s.id_subject} "
+            f"{s.id_day} {s.lesson_number}]")
+
 def list(request):
 
 	if('day' in request.GET and 'class_name' in request.GET):
@@ -10,6 +15,6 @@ def list(request):
 		rclass = request.GET['class_name']
 		classn = models.Class_Name.objects.all().filter(name=rclass)
 		Schedule = models.Schedule.objects.all().filter(id_day=rday)
-		return HttpResponse(Schedule)
+		return HttpResponse([print_schedule(s) for s in Schedule])
 	else:
 		return HttpResponse(f"Not enough parametrs")
