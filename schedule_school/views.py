96487678f589675f7a8b5f6cb29f89cf69f007e9
logerk46 <logerk15a@gmail.com>
2018-11-10 13:51:26 +0200

    changes in view

--- schedule_school/views.py
+++ schedule_school/views.py
@@ -5,16 +5,19 @@ from . import models
 # Create your views here.
 
 def print_schedule(s):
-    return (f"[{s.room_number} {s.id_teacher} {s.id_class} {s.id_subject} "
+    return (f"[{s.room_number}{s.id_class} {s.id_subject} "
             f"{s.id_day} {s.lesson_number}]")
 
 def list(request):
-
-	if('day' in request.GET and 'class_name' in request.GET):
-		rday = request.GET['day']
+	if('class_name' in request.GET):
+		schedule_list = {}
 		rclass = request.GET['class_name']
 		classn = models.Class_Name.objects.all().filter(name=rclass)
-		Schedule = models.Schedule.objects.all().filter(id_day=rday)
-		return HttpResponse([print_schedule(s) for s in Schedule])
+		classn = models.Class_Name.objects.all().filter(name=rclass)
+		for day_id in range(1, 6):
+			Schedule = models.Schedule.objects.all().filter(id_day=day_id, id_class=classn[0].id)
+			schedule_list.append(Sc
+		return HttpResponse(schedule_list[0][4].lesson_number)
+		# return HttpResponse([print_schedule(s) for s in Schedule])
 	else:
 		return HttpResponse(f"Not enough parametrs")
